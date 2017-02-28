749fb3e0a9bc88220e950da31ecb62b881d81daf
Grzegorz <bugala@mlsdev.com>
2017-02-28 08:54:16 +0100

    Add TheMovieDb class to services in the app and make specs passing

---
+++ app/services/the_movie_db.rb
@@ -0,0 +1,25 @@
+require 'httparty'
+
+class TheMovieDb
+  attr_reader :options
+
+  include HTTParty
+  base_uri "api.themoviedb.org/3/"
+  default_params :api_key => ENV['THE_MOVIEDB_V3']
+
+  def initialize movie
+    @options = { query: { query: movie.title, year: movie.released_at } }
+  end
+
+  def movie_details
+    movie_from_api ||= movie_search.parsed_response['results']
+
+    OpenStruct.new(movie_from_api.first)
+  end
+
+  private
+
+  def movie_search
+    @movie_search ||= self.class.get("/search/movie", @options )
+  end
+end
