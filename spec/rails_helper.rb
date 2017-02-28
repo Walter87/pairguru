ca7cf83393a7c3654d7d90aa6b107d027287261a
Grzegorz <bugala@mlsdev.com>
2017-02-28 09:24:04 +0100

    Fix simple_cov not showing coverage for user model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,21 +1,21 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-require 'pry'
-require 'capybara/rails'
 require 'simplecov'
-require 'shoulda/matchers'
+
 SimpleCov.start 'rails' do
   add_group 'Decorators', 'app/decorators'
-
-  add_group 'Observers', 'app/observers'
 
   add_group 'Mailers', 'app/mailers'
 
   add_group 'Services', 'app/services'
 end
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'pry'
+require 'capybara/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
