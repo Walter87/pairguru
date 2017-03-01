47830dc92509e0a9d8d5b8e9f9980263b961d1b2
Grzegorz <bugala@mlsdev.com>
2017-03-01 22:49:36 +0100

    Update base controller for api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,13 +1,3 @@
 class Api::V1::BaseController < ApplicationController
-  rescue_from ActionController::ParameterMissing do
-    head 400
-  end
-
-  rescue_from ActiveRecord::RecordInvalid do
-    head 422
-  end
-
-  rescue_from ActiveRecord::RecordNotFound do
-    head 404
-  end
+  skip_before_action :verify_authenticity_token
 end
