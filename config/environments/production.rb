f628dea0b7d82167d119bca127e61612310324be
Grzegorz <bugala@mlsdev.com>
2017-03-02 11:06:20 +0100

    Update mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,17 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => "intense-brook-24091.heroku.com" }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'website.com',
+    user_name:            'user@website.com',
+    password:             'password',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
